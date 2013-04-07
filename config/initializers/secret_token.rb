eadb618b9b725720d47c906444487af9390337ac
田井 成紀 <taishigeki@tai-no-MacBook-Air.local>
2013-04-07 22:00:03 +0900

    blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoshiwaraBlog::Application.config.secret_token = 'd6f3a30cd5e5dd538193a5b8d95217741308b7acf9da7b41d78f218b5527aa9ff682d4adbc52557e16df7210478353882ba542eefe1de2d4bf01851d9fa5c649'
