f0486a4e79c0766572eef1ce7a33edda7b2c3913
田井 成紀 <taishigeki@tai-no-MacBook-Air.local>
2013-04-08 23:50:46 +0900

    aaaa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title
+  
+  validates :title , :presence => true
+  validates :body , :presence => true
+  validates :body , :length => { :minimum => 5 }  
 end
